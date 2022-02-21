77558a63d49bd4b429acece1f30e47a35c43758d
JadeDragon <OlegOvNiNo@mail.ru>
2022-02-21 23:50:26 +0300

    Алгоритм анализа кол-ва знаков player в ячейках ключ. массива (185-197)

--- myScript.js
+++ myScript.js
@@ -61,24 +61,27 @@ class Game {
     this.init();
   }
 
+  // изменение ячейки ключевого массива
   changeCheckArrayCell(id, player, num) {
     // достаем значение ячейки num из ключевого массива и сплитим
     // в самом начале игры получается ['*','*', ...] размором this.size
     let arrayFromNum = this.checkArray[num].split('');
     // заменяем в сплит-массиве необходимую позицию на знак player
-    arrayFromNum.splice(Math.floor(id % this.size), 1, player);
+    arrayFromNum.splice(Math.floor(id / this.size), 1, player);
+    // console.log('Позиция', Math.floor(id / this.size));
     // склеиваем обратно сплит-массив в ячейку num
     // и запихиваем обратно в ключевом массиве на туже позицию
     this.checkArray[num] = arrayFromNum.join('');
     // console.log(`checkArray[${rowNum}] = ${this.checkArray[rowNum]}`);
   }
-
-  // ф-ция записи хода в клучевой массив
-  makeMove(id /*номер ячейки*/, player) {
+  // запись хода в ключевой массив
+  makeMove(id, player) {
     let rowNum = Math.floor(id / this.size); // номер строки в к-рую сходил player
+    // console.log('Строка', rowNum);
     this.changeCheckArrayCell(id, player, rowNum);
 
     let columnNum = (id % this.size) + this.size; // номер столбца в к-рый сходил чел
+    // console.log('Столбец', columnNum);
     this.changeCheckArrayCell(id, player, columnNum);
 
     // анализируем все возможные диагонали
@@ -92,6 +95,7 @@ class Game {
       ) {
         // позиция в ключ. массиве = 14+i (при victoryNum = 7)
         let checkArrayPos = 2 * this.size + (id % (this.size + 1));
+        // console.log('Диагональ(1 if)', checkArrayPos);
         this.changeCheckArrayCell(id, player, checkArrayPos);
       }
       if (
@@ -103,9 +107,9 @@ class Game {
         // позиция в ключ. массиве = 17+i (при victoryNum = 7)
         let checkArrayPos =
           2 * this.size + (this.size - (this.victoryNum - 1)) + i;
+        // console.log('Диагональ(2 if)', checkArrayPos);
         this.changeCheckArrayCell(id, player, checkArrayPos);
       }
-
       if (
         // главная диагональ/ (при i=0) и побочные диагонали// НИЖЕ
         id % (this.size - 1) === i &&
@@ -115,6 +119,7 @@ class Game {
         // позиция в ключ. массиве = 19+i (при victoryNum = 7)
         let checkArrayPos =
           2 * this.size + 2 * (this.size - this.victoryNum) + 1 + i;
+        // console.log('Диагональ(3 if)', checkArrayPos);
         this.changeCheckArrayCell(id, player, checkArrayPos);
       }
       if (
@@ -126,24 +131,24 @@ class Game {
         // позиция в ключ. массиве = 22+i (при victoryNum = 7)
         // let checkArrayPos = 2 * this.size + (this.size + 1) + i;
         let checkArrayPos = this.victoryNum * (this.size - 3) + 2 + i;
+        // console.log('Диагональ(4 if)', checkArrayPos);
         this.changeCheckArrayCell(id, player, checkArrayPos);
       }
     }
-    console.log(this.checkArray);
+    // console.log(this.checkArray);
   }
-
+  // ход человека (срабатывает при клике на ячейке поля)
   humanPlay() {
-    // срабатывает при клике на ячейке
     return (e) => {
       this.turnCount += 1;
       const id = +e.target.getAttribute('data-id'); // получаем ID ячейки
-      this.board[id] = huPlayer; // записываем в позицию массива символ игрока
+      this.board[id] = huPlayer; // записываем в позицию массива board символ игрока
       this.cellList[id].innerHTML = `<span>${huPlayer}</span>`; // добавляем в DOM символ игрока
       if (this.turnCount >= this.limit) {
         result.innerHTML = `<h3> Draw! </h3>`;
         return;
       }
-      this.makeMove(id, huPlayer);
+      this.makeMove(id, huPlayer); // запись хода в ключевой массив
       // let indices = []; // массив со всеми ячейками, к-рые включают знак player
       // let ind = board.indexOf(player);
       // while (ind != -1) {
@@ -152,72 +157,87 @@ class Game {
       // }
       // console.log(`indices = `, indices);
 
-      if (this.checkWinner(this.board, huPlayer)) {
+      if (this.checkWinner(/* this.board, */ huPlayer)) {
         // проверка на победную позицию
         result.innerHTML = `<h3>You win!</h3>`;
         return;
       }
-
-      // this.makeAiTurn();
+      this.makeAiTurn(huPlayer);
     };
   }
 
-  makeAiTurn() {
-    this.turnCount += 1;
-    const bestMove = this.minimax(this.board, aiPlayer);
-    console.log(bestMove);
-    this.board[bestMove.index] = aiPlayer;
-    this.cellList[bestMove.index].innerHTML = `<span>${aiPlayer}</span>`;
-    if (this.turnCount >= this.limit) {
-      result.innerHTML = `<h3> Draw! </h3>`;
-      return;
-    }
-    if (this.checkWinner(this.board, aiPlayer)) {
-      result.innerHTML = `<h3>AI win!</h3>`;
-      return;
-    }
+  makeAiTurn(player) {
+    // this.turnCount += 1;
+    // const bestMove = this.minimax(this.board, aiPlayer);
+    // console.log(bestMove);
+    // this.board[bestMove.index] = aiPlayer;
+    // this.cellList[bestMove.index].innerHTML = `<span>${aiPlayer}</span>`;
+    // if (this.turnCount >= this.limit) {
+    //   result.innerHTML = `<h3> Draw! </h3>`;
+    //   return;
+    // }
+    // if (this.checkWinner(this.board, aiPlayer)) {
+    //   result.innerHTML = `<h3>AI win!</h3>`;
+    //   return;
+    // }
+    // console.log(this.checkArray);
+
+    let indices = []; // номера ячеек ключ. массива, к-рые имеют 3 знака player или более
+    this.checkArray.forEach((cell, index) => {
+      if (cell.includes(player)) {
+        let count = 0;
+        let ind = cell.indexOf(player);
+        while (ind != -1 && count <= this.victoryNum - 2) {
+          ind = cell.indexOf(player, ind + 1);
+          count++;
+        }
+        count >= this.victoryNum - 2 ? indices.push(index) : null; // отсекаем ячейки где знаков player < 3
+      }
+    });
+    console.log('indices = ', indices);
   }
 
-  checkWinner(board, player) {
-    const checkAvailability = (arr, val) => {
+  // проверка ключевого массива на победное состояние
+  checkWinner(/* board, */ player) {
+    const checkWin = (arr, value) => {
       return arr.some((arrVal) => {
-        return arrVal.includes(val);
+        return arrVal.includes(value);
       });
     };
 
-    if (checkAvailability(this.checkArray, 'O'.repeat(this.victoryNum))) {
+    if (checkWin(this.checkArray, player.repeat(this.victoryNum))) {
       return true;
     } else {
       return false;
     }
   }
 
-  findEmptyCells(board) {
+  findEmptyPosition(cell) {
     // выделяес из всей доски пустые клетки
     return board.filter((cell) => cell !== huPlayer && cell !== aiPlayer);
   }
-
+  // выделяет из всей доски клетки чела
   findHuCells(board) {
-    // выделяес из всей доски  клетки чела
-    // return board.indexOf(huPlayer);
     let huCells = [];
     board.forEach((el, i) => {
       el === huPlayer ? huCells.push(i) : null;
     });
     return huCells;
   }
-
+  // выделяет из всей доски клетки AI
   findAiCells(board) {
-    // выделяес из всей доски клетки AI
     let aiCells = [];
     board.forEach((el, i) => {
       el === aiPlayer ? aiCells.push(i) : null;
     });
     return aiCells;
   }
-
   // функция анализа и предсказания ходов и победных позиций
-  minimax(board, player) {
+  // myMiniMax(cell, player) {
+  //   const emptyPosition =
+  // }
+
+  /*   minimax(board, player) {
     const emptyCells = this.findEmptyCells(board); // массив пустых клеток
     // console.log('emptyCells =', emptyCells);
     if (this.checkWinner(board, huPlayer)) {
@@ -233,10 +253,10 @@ class Game {
       return { score: 0 };
     }
 
-    // доходим до этого момента в коде если предидущие 3 блока if вернули false
+    // доходим до этого момента если предидущие 3 блока if вернули false
     let moves = []; // массив для всех возможных ходов
     for (let i = 0; i < emptyCells.length; i++) {
-      let move = {}; // параметры конкретного хода
+      let move = {}; // параметры конкретного хода (индекс ячейки + score)
       board[emptyCells[i]] = player; // делаем ход за конкретного игрока(чел или комп)
       move.index = emptyCells[i]; // порядковый номер ячейки для послед. хода
       if (player === huPlayer) {
@@ -250,8 +270,8 @@ class Game {
       board[emptyCells[i]] = move.index;
       moves.push(move);
     }
-    let bestMove;
 
+    let bestMove;
     // на данный момент кода массив moves полностью заполнен всеми возможными ходами
     // выбор наилучшего хода, если очередь хода за компом
     if (player === aiPlayer) {
@@ -277,9 +297,8 @@ class Game {
         }
       }
     }
-
     return moves[bestMove]; // возвращаем из цикла св-ва лучшего хода
-  }
+  } */
 }
 
 new Game(19); // запускаем игру
